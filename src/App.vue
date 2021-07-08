01cdf8d1d3719f8cc827f501f0fd7dbbb5b84080
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-07-08 16:20:26 +0200

    add: layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Vetrina/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vetrina from './components/Vetrina.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Vetrina
   }
 }
 </script>
 
 <style lang="scss">
+@import '@/style/general.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+      <a href="https://www.spotify.com/it" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/7/71/Spotify.png" alt="Spotify"></a>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+div{
+    height: 70px;
+    background-color: #2E3A46;
+    a{
+        margin: 10px 10px 0;
+        img{
+            width: 70px;
+            border-radius: 50%;
+        }
+    }
+}
+
+</style>
